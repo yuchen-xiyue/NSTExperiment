8030bcb943c597bf72e47b6d74aa6d5430795ec7
yuchen-xiyue <yuchen.wu.xiyue@gmail.com>
2021-10-19 02:09:49 +0100

    fixed __main__ error

--- nst_/main.py
+++ nst_/main.py
@@ -7,7 +7,7 @@ parser.add_argument('-i', '--iteration', type=int, default=500, help='Max iterat
 parser.add_argument('-a', '--alpha', type=int, default=-2, help='The exponential index of content loss weight')
 parser.add_argument('-b', '--beta',  type=int, default=9,  help='The exponential index of style loss weight')
 parser.add_argument('-l', '--learnrate', type=float, default=1, help='Learning Rate. ')
-if __name__ == "main":
+if __name__ == "__main__":
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     args = parser.parse_args()
     process = Optimizing(device, alpha=10**args.alpha, beta=10**args.beta, iterations=args.iteration, lr=args.learnrate)
