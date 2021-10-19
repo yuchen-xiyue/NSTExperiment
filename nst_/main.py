ffdf5b0e9428063f007285ddb16cf1ab1d784550
yuchen-xiyue <yuchen.wu.xiyue@gmail.com>
2021-10-19 01:50:37 +0100

    fixed scope error

--- nst_/main.py
+++ nst_/main.py
@@ -1,21 +1,15 @@
 # for command line
-from training import *
+import torch
+from training import Optimizing
 from argparse import ArgumentParser
 parser = ArgumentParser()
-parser.add_argument('-i', '--iteration', type=int, help='Max iteration for optimization')
+parser.add_argument('-i', '--iteration', type=int, default=500, help='Max iteration for optimization')
 parser.add_argument('-a', '--alpha', type=int, default=-2, help='The exponential index of content loss weight')
 parser.add_argument('-b', '--beta',  type=int, default=9,  help='The exponential index of style loss weight')
-parser.add_argument('-l', '--learnrate', type=float, help='Learning Rate. ')
-
-global STYLE_WEIGHT, CONTENT_WEIGHT
-args = parser.parse_args()
-if args.iteration:
-    global MAX_ITERATIONS
-    MAX_ITERATIONS = args.iteration
-alpha, beta = 10**args.alpha, 10**args.beta
-CONTENT_WEIGHT, STYLE_WEIGHT = alpha * CONTENT_WEIGHT, beta * STYLE_WEIGHT
-if args.learnrate:
-    global LEARNING_RATE
-    LEARNING_RATE = args.learnrate
-result_image = style_transfer()
-show_img(result_image).save(RESULT_PATH)
+parser.add_argument('-l', '--learnrate', type=float, default=1, help='Learning Rate. ')
+if __name__ == "main":
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    args = parser.parse_args()
+    process = Optimizing(device, alpha=10**args.alpha, beta=10**args.beta, iterations=args.iteration, lr=args.learnrate)
+    result_image = process.style_transfer()
+    process.save_result()
--- nst_/training.py
+++ nst_/training.py
@@ -3,49 +3,57 @@ from model import *
 from utils import *
 
 Tensor = torch.tensor
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-STYLE_WEIGHT = np.array([1.50, 0.80, 0.25, 0.25, 0.25]) # what if make this vary? the later, the haigher for the deeper
-CONTENT_WEIGHT = np.array([1])
-LEARNING_RATE = 1
-CONTENT_PATH = 'images/content.jpg'
-STYLE_PATH   = 'images/style.jpg'
-RESULT_PATH  = 'result.jpg'
+class Optimizing:
+    def __init__(self, device, alpha=-2, beta=9, iterations=500, lr=1):
+        self.STYLE_WEIGHT = beta * np.array([1.50, 0.80, 0.25, 0.25, 0.25]) # what if make this vary? the later, the haigher for the deeper
+        self.CONTENT_WEIGHT = alpha * np.array([1])
+        self.LEARNING_RATE = lr
+        self.CONTENT_PATH = 'images/content.jpg'
+        self.STYLE_PATH   = 'images/style.jpg'
+        self.RESULT_PATH  = 'result.jpg'
+        self.MAX_ITERATIONS = iterations
+        self.SHOW_ITERATIONS = 50
+        self.DEVICE = device
 
-MAX_ITERATIONS = 500
-SHOW_ITERATIONS = 50
+    def load_images(self, from_content=True):
+        content_image = load_img(self.CONTENT_PATH).to(self.DEVICE)
+        style_image   = load_img(self.STYLE_PATH).to(self.DEVICE)
+        if from_content:
+            result_image = content_image.clone()
+        result_image.requires_grad = True
 
-content_image = load_img(CONTENT_PATH).to(device)
-style_image   = load_img(STYLE_PATH).to(device)
-result_image  = content_image.clone()
+        return content_image, style_image, result_image
 
-result_image.requires_grad = True
+    def save_result(self):
+        show_img(self.result_image).save(self.RESULT_PATH)
 
+    def style_transfer(self):
+        model = NSTModel()
+        model.to(self.DEVICE)
+        content_image, style_image, result_image = self.load_images()
+        optim = torch.optim.LBFGS([result_image], lr=self.LEARNING_RATE)
 
-def style_transfer():
-    model = NSTModel()
-    model.to(device)
-    optim = torch.optim.LBFGS([result_image], lr=LEARNING_RATE)
+        style_targets   = [GramMatrix()(A).detach() for A in model(style_image)[0]]
+        content_targets = [A.detach() for A in model(content_image)[1]]
 
-    style_targets   = [GramMatrix()(A).detach() for A in model(style_image)[0]]
-    content_targets = [A.detach() for A in model(content_image)[1]]
+        n_iterations = [0] # avoid error due to locality
+        while n_iterations[0] <= self.MAX_ITERATIONS:
+            def closure():
+                optim.zero_grad()
+                style_outputs, content_outputs = model(result_image)
+                content_loss = [self.CONTENT_WEIGHT[i] * ContentLoss()(Y, content_targets[i]) for i, Y in enumerate(content_outputs)]
+                style_loss   = [self.STYLE_WEIGHT[i]   * GramMSELoss()(Y, style_targets[i]) for i, Y in enumerate(style_outputs)]
 
-    n_iterations = [0] # avoid error due to locality
-    while n_iterations[0] <= MAX_ITERATIONS:
-        def closure():
-            optim.zero_grad()
-            style_outputs, content_outputs = model(result_image)
-            content_loss = [CONTENT_WEIGHT[i] * ContentLoss()(Y, content_targets[i]) for i, Y in enumerate(content_outputs)]
-            style_loss   = [STYLE_WEIGHT[i] * GramMSELoss()(Y, style_targets[i]) for i, Y in enumerate(style_outputs)]
+                loss = sum(content_loss + style_loss)
+                loss.backward()
 
-            loss = sum(content_loss + style_loss)
-            loss.backward()
+                n_iterations[0] += 1
+                if n_iterations[0] % self.SHOW_ITERATIONS == (self.SHOW_ITERATIONS - 1):
+                    print('Iteration: %d, Style Loss: %.4f, Content Loss: %.4f' % (n_iterations[0], sum(style_loss).item(), sum(content_loss).item()))
+                return loss
 
-            n_iterations[0] += 1
-            if n_iterations[0] % SHOW_ITERATIONS == (SHOW_ITERATIONS - 1):
-                print('Iteration: %d, Style Loss: %.4f, Content Loss: %.4f' % (n_iterations[0], sum(style_loss).item(), sum(content_loss).item()))
-            return loss
-
-        optim.step(closure)
-    return result_image
+            optim.step(closure)
+        self.result_image = result_image
+        return result_image
